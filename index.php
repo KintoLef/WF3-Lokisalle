40fe94b4f3cbe551f05a40f3e598ba1ee07324ec
Jordan MULLER <jordanmuller29@gmail.com>
2017-07-20 12:44:34 +0200

    Retest

--- index.php
+++ index.php
@@ -5,21 +5,14 @@ require_once("inc/head.inc.php");
 require_once("inc/nav.inc.php");
 ?>
 
-<<<<<<< HEAD
-        <title>Lokisalle</title>
-    </head>
-    <body>
-        <header>
-           
-        </header>
-=======
 <div class="container">
->>>>>>> 6ce1241dbb9a48e399512fc8de98574f133a0183
 
     <div class="starter-template">
         <h1>Bienvenue sur Lokisalle</h1>
     </div>
     <?= $message; // cette balise php inclus un echo (equivalent à la ligne du dessus) ?>
+
+    
 
 </div><!-- /.container -->
         
