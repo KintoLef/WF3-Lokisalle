a7079346958e73e67e75f3f9f87d4fc3c612e536
KintoLef <quinto_lefevre@hotmail.com>
2017-07-20 14:45:07 +0200

    Création de la page profil

--- connexion.php
+++ connexion.php
@@ -7,10 +7,10 @@ if(isset($_GET['action']) && $_GET['action'] == 'deconnexion')
 }
 
 // vérification si l'utilisateur est connecté si oui on le redirige sur profil.php
-if(utilisateur_connecte())
-{
-    header('location:profil.php');
-}
+// if(utilisateur_connecte())
+// {
+//     header('location:profil.php');
+// }
 
 // vérification de l'existence des indices du formulaire
 if(isset($_POST['pseudo']) && isset($_POST['mdp']))
@@ -29,28 +29,17 @@ if(isset($_POST['pseudo']) && isset($_POST['mdp']))
         $info_utilisateur = $verif_connexion->fetch(PDO::FETCH_ASSOC);
         
         // on place toutes les informations de l'utilisateur dans la session sauf le mdp
-        $_SESSION['utilisateur'] = array();
-        $_SESSION['utilisateur']['id_membre'] = $info_utilisateur['id_membre'];
-        $_SESSION['utilisateur']['pseudo'] = $info_utilisateur['pseudo'];
-        $_SESSION['utilisateur']['nom'] = $info_utilisateur['nom'];
-        $_SESSION['utilisateur']['prenom'] = $info_utilisateur['prenom'];
-        $_SESSION['utilisateur']['email'] = $info_utilisateur['email'];
-        $_SESSION['utilisateur']['civilite'] = $info_utilisateur['civilite'];        
-        $_SESSION['utilisateur']['statut'] = $info_utilisateur['statut'];
-        $_SESSION['utilisateur']['statut'] = $info_utilisateur['statut'];
+        $_SESSION['membre'] = array();
+        $_SESSION['membre']['id_membre'] = $info_utilisateur['id_membre'];
+        $_SESSION['membre']['pseudo'] = $info_utilisateur['pseudo'];
+        $_SESSION['membre']['nom'] = $info_utilisateur['nom'];
+        $_SESSION['membre']['prenom'] = $info_utilisateur['prenom'];
+        $_SESSION['membre']['email'] = $info_utilisateur['email'];
+        $_SESSION['membre']['civilite'] = $info_utilisateur['civilite'];        
+        $_SESSION['membre']['statut'] = $info_utilisateur['statut'];
+        $_SESSION['membre']['statut'] = $info_utilisateur['statut'];
         
-
         header('location:profil.php');
-
-        // même chose avec un foreach
-        // $_SESSION['utilisateur'] = array();
-        // foreach($info_utilisateur AS $indice => $valeur)
-        // {
-        //     if($indice != 'mdp')
-        //     {
-        //         $_SESSION['utilisateur'][$indice] = $valeur;
-        //     }            
-        // }
     }
     else {
         $message .= '<div class="alert alert-danger" role="alert">Attention, les informations saisies sont erronées !!</div>';
