a8d963f01a74178f162418b974c59c36643c87f3
jordanmuller <jordanmuller29@gmail.com>
2017-07-21 00:31:47 +0200

    Travil du soir

--- admin/gestion_membre.php
+++ admin/gestion_membre.php
@@ -60,9 +60,67 @@ if(utilisateur_admin())
 
     $message .= '</table>';
 
+    // Si l'on clique sur le bouton modifier
     if(isset($_GET['modifier']) && !empty($_GET['id_membre']) && is_numeric($_GET['id_membre']))
     {
-        
+        // Affichage des champs input 
+        $message .= '
+        <form method="post" action="">
+            <div class="row">
+                <div class="col-md-6">
+                    <label class="gestion_membre" for="pseudo">Pseudo</label>
+                    <div class="input-group">
+                        <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user"></span></span>
+                        <input type="text" class="form-control" placeholder="pseudo" id="pseudo" name="pseudo" aria-describedby="basic-addon1">
+                    </div>
+                    <label class="gestion_membre" for="mdp">Mot de passe</label>
+                    <div class="input-group">
+                        <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-lock"></span></span>
+                        <input type="text" class="form-control" placeholder="mot de passe" id="mdp" name="mdp" aria-describedby="basic-addon1">
+                    </div>
+                    <label class="gestion_membre" for="nom">Nom</label>
+                    <div class="input-group">
+                        <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-pencil"></span></span>
+                        <input type="text" class="form-control" placeholder="votre nom" name="nom" id="nom" aria-describedby="basic-addon1">
+                    </div>
+                    <label class="gestion_membre" for="prenom">Prénom</label>
+                    <div class="input-group">
+                        <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-pencil"></span></span>
+                        <input type="text" class="form-control" placeholder="votre prénom" name="prenom" id="prenom" aria-describedby="basic-addon1">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <label class="gestion_membre" for="email">Email</label>
+                    <div class="input-group">
+                        <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-envelope"></span></span>
+                        <input type="text" class="form-control" placeholder="votre email" name="email" id="email" aria-describedby="basic-addon1">
+                    </div>
+                    
+                    <label class="gestion_membre" for="civilite">Civilité</label>
+                    <div class="input-group">
+                        <select name="civilite" id="civilite">                    
+                            <option value="m">Homme</option>
+                            <option value="f">Femme</option>                                                                
+                        </select>
+                    </div>
+                    <label class="gestion_membre" for="statut">Statut</label>
+                    <div class="input-group">
+                        <select name="statut" id="statut">                    
+                            <option value="0">Membre</option>
+                            <option value="1">Administrateur</option>                                                                
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-membre pull-right">Enregistrer</button>    
+                </div>
+            </div>
+            </form>'
+            ;
+
+
+        $id_membre = $_GET['id_membre'];
+        $membre_a_modifier = $pdo->prepare("SELECT * FROM membre WHERE id_membre = :id_membre");
+        $membre_a_modifier->bindParam(":id_membre", $id_membre, PDO::PARAM_STR);
+        $membre_a_modifier->execute();
     }
 
 } else {
@@ -72,6 +130,7 @@ if(utilisateur_admin())
 
 require_once("../inc/head.inc.php");
 require_once("../inc/nav.inc.php");
+echo '<pre>'; print_r($_POST); echo '</pre>';
 ?>
 
 <div class="container">
@@ -79,7 +138,6 @@ require_once("../inc/nav.inc.php");
     <div class="starter-template">
         <h1>Gestion des membres</h1>
     </div>
-    <hr />
     <?= $message; // cette balise php inclus un echo (equivalent à la ligne du dessus) ?>
 
     
